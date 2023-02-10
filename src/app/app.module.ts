692acacb35532d80d7e1a20463a7138b47c04eec
Miguel <78430957+lMiguelVSl@users.noreply.github.com>
2023-02-09 21:22:16 -0500

    Basic component implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent } from './shared/warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './shared/success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    ServerComponent
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
