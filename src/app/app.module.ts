3f09eb5a08cf901b27d98ef6423c32ff7be462d4
Miguel <78430957+lMiguelVSl@users.noreply.github.com>
2023-02-09 20:48:07 -0500

    Basic components behavior and creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ServerComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
