562d1dd3a78a1f06896ab937c33b1ab6f67689ef
Miguel <78430957+lMiguelVSl@users.noreply.github.com>
2023-03-05 12:19:43 -0500

    assignement refactoring

--- src/app/components/assignment/assignment.component.ts
+++ src/app/components/assignment/assignment.component.ts
@@ -9,12 +9,16 @@ export class AssignmentComponent {
 
   userName: string = '';
   allowSetButton: boolean = true;
+  inputValue: string = '';
 
   onUpdateServerName(event: any) {
     this.userName = event.target.value;
+    if (this.userName != '') this.allowSetButton = false;
   }
 
   onResetUserName() {
+    this.allowSetButton = true;
     if (this.userName != '') this.userName = '';
+    this.inputValue = '';
   }
 }
